c73dc470321bbdf8eb3719d2816e5b72f0d71687
JulioKoepsel <julio.koepsel@gmail.com>
2023-12-11 21:25:09 -0300

    ArvoreB

--- src/arvores/TesteArvore.java
+++
@@ -1,7 +0,0 @@
-package arvores;
-
-public class TesteArvore {
-    public static void main(String[] args) {
-        
-    }
-}
---
+++ src/arvores/TesteArvoreB.java
@@ -0,0 +1,27 @@
+package arvores;
+
+public class TesteArvoreB {
+    public static void main(String[] args) {
+
+        ArvoreB arvore = new ArvoreB(3);
+
+        arvore.inserir(8);
+        arvore.inserir(9);
+        arvore.inserir(10);
+        arvore.inserir(11);
+        arvore.inserir(15);
+        arvore.inserir(20);
+        arvore.inserir(17);
+
+        arvore.mostrar();
+
+        System.out.println(arvore.buscar(10));
+
+        arvore.remover(10);
+
+        arvore.mostrar();
+
+        System.out.println(arvore.buscar(10));
+
+    }
+}
